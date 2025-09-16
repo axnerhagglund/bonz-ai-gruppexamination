bcaeb6813158058173c35b7ccc13fc0e6c1d910d
isabellarssonn <isabel.larsson@hotmail.com>
2025-09-16 17:38:47 +0200

    first draft of getAllRooms

--- functions/getAllRooms/index.mjs
+++ functions/getAllRooms/index.mjs
@@ -1,8 +1,36 @@
+import { client } from "../../services/db.mjs";
+import { QueryCommand } from "@aws-sdk/client-dynamodb";
+
 export const handler = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Go Serverless v4! Your function executed successfully!",
-    }),
-  };
+  try {
+    const command = new QueryCommand({
+      TableName: "RoomBookTable",
+      IndexName: "bookingIndex",
+      KeyConditionExpression: "booking = :booking AND begins_with(pk, :pk)",
+      ExpressionAttributeValues: {
+        ":booking": { S: "BOOKING" },
+        ":pk": { S: "BOOKING#" },
+      },
+    });
+
+    const bookings = await client.send(command);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        success: true,
+        bookings,
+      }),
+    };
+
+  } catch (error) {
+    console.log(error) 
+    return {
+        statusCode: 500,
+        body: JSON.stringify({
+            success: false,
+            message: "Couldn't get all bookings"
+        }),
+    };
+  }
 };
